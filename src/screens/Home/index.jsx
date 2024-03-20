74d781f902a798a80c587484c338a49eac3e8aad
Victormattos564 <victormaria@aluno.senai.br>
2024-03-20 10:30:27 -0300

    inciando

---
+++ src/screens/Home/index.jsx
@@ -0,0 +1,20 @@
+import { View } from "react-native";
+
+import styles from "./styles";
+import Title from "../../components/Title";
+import { user } from "../../data/Profile";
+import TouchButton from "../../components/TouchButton";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Title title="Home" />
+
+      <TouchButton route="Profile" title="Go to Profile" data={user} />
+
+      <TouchButton route="Category" title="Go to Category" />
+
+      <TouchButton route="Users" title="Go to Users" />
+    </View>
+  );
+}
